9d976a41ea9ffefe7511ae204835ce3aebfcff94
Dulio Denis <dulio.denis@yahoo.com>
2017-02-06 16:35:47 -0500

    Implement a shopping list app

--- basics/shopping_list.py
+++ basics/shopping_list.py
@@ -11,3 +11,27 @@
 #  - Enter the word DONE - in all CAPS - to quit the program
 #  - And, once I quit, I want the app to show me everything that's on my List
 #
+# make a list to hold our items
+shopping_list = []
+
+# print out instructions on how to use the app
+print("What should we do today?")
+print("Enter 'DONE' to stop adding items.")
+
+
+while True:
+    # ask for new items
+    new_item = input("> ")
+
+    # be able to quit the app
+    if new_item == 'DONE':
+        break
+    else:
+        # add new items to our List
+        shopping_list.append(new_item)
+
+# print out the List
+print("Here is your list:")
+
+for item in shopping_list:
+    print(item)
