eb7ba0f53402893f9e24f5d7d00a456dae436ed3
Dulio Denis <dulio.denis@yahoo.com>
2017-02-08 11:13:28 -0500

    Refactored game with try/except and more options

--- 1_basics/number_guessor.py
+++ 1_basics/number_guessor.py
@@ -9,19 +9,51 @@
 #  The computer tells you if your guess is too low or high.
 # ------------------------------------------------
 # v1: basic functionality
+# v2: limit the number of guesses, catch non-integer submissions,
+#     print too low or too high messages for bad guesses,
+#     let people play again
+#
 import random
-# generate a random number between 1 and 10
-secret_num = random.randint(1, 10)
 
-# set up the game loop
-while True:
-    # get a number guess from the player
-    guess = int(input("Guess a number between 1 and 10: "))
+def game():
+    # generate a random number between 1 and 10
+    secret_num = random.randint(1, 10)
 
-    # compare guess to the secret number
-    if guess == secret_num:
-        print("You got it! My number was {}".format(secret_num))
-        break
+    # we'll give the player 5 tries
+    tries = 5
+
+    # set up the game loop
+    while tries > 0:
+        # get a number guess from the player
+        # use try / except to catch non-integer guesses
+        try:
+            guess = int(input("Guess a number between 1 and 10. I'll give you {} tries: ".format(tries)))
+        except ValueError:
+            print("You must enter an integer value as a guess.")
+            continue
+
+        # compare guess to the secret number
+        if guess == secret_num:
+            print("You got it! My number was {}".format(secret_num))
+            break
+        else:
+            tries -= 1
+            if tries == 0:
+                print("Better luck next time.")
+
+            else:
+                # print too low or too high
+                if guess > secret_num:
+                    print("Too high.")
+                else:
+                    print("Too low.")
+                print("Try again. You have {} tries left".format(tries))
     else:
-        print("Try again.")
-        # print hit or miss
+        print("You didn't get it. My number was {}.".format(secret_num))
+    play_again = input("Do you want to play again? Y/n ")
+    if play_again.lower() != 'n':
+        game()
+    else:
+        print("Thanks for playing.")
+
+game()
